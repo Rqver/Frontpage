0ecca7861e8182a73748a5c3f0c79ebb332ed8e9
rqver <hunterynz@gmail.com>
2025-12-15 21:51:19 +1300

    feat: Newsroom

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import {getTopTheSpinoffStories} from "./modules/thespinoff.ts";
 import {getTopRNZStories} from "./modules/rnz.ts";
 import {getTopHeraldStories} from "./modules/herald.ts";
+import {getTopNewsroomStories} from "./modules/newsroom.ts";
 
-console.log(await getTopHeraldStories())
+console.log(await getTopNewsroomStories())
---
+++ modules/newsroom.ts
@@ -0,0 +1,25 @@
+import * as cheerio from 'npm:cheerio';
+import { Story } from "../types.ts";
+
+export async function getTopNewsroomStories(): Promise<Story[]> {
+    const res = await fetch("https://newsroom.co.nz/");
+    const text = await res.text();
+
+    const $ = cheerio.load(text);
+    const stories: Story[] = [];
+
+    const articleElements = $(".wp-block-newspack-blocks-homepage-articles article");
+
+    for (const articleElement of articleElements){
+        const element = $(articleElement);
+        const headlineTag = element.find(".entry-title a").first();
+
+        stories.push({
+            title: headlineTag.text().trim(),
+            isBreaking: false,
+            link: headlineTag.attr("href") || ""
+        })
+    }
+
+    return stories;
+}
